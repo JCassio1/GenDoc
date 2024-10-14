51db8a6ab75bc1818d47cfc32408363155c53eb2
JCassio1 <Joselsondias@hotmail.com>
2024-10-14 23:23:13 +0100

    Add logo to navbar

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '../ui/modeToggler'
+import Image from 'next/image'
+import logoDark from '../../app/assets/GenDoc_logo_for_dark.png'
 
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between p-4 bg-background'>
       <div className='flex items-center'>
         <Link href='/' className='text-2xl font-bold text-foreground'>
-          Logo
+          <Image src={logoDark} width={100} height={100} alt='GenDoc Logo' />
         </Link>
       </div>
       <div className='flex items-center space-x-4'>
