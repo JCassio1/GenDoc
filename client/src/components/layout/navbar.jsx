dd22d55debb8a7f31c6bf0287964893a13aa821d
JCassio1 <Joselsondias@hotmail.com>
2024-10-30 22:36:56 +0000

    Change logo based on theme setting

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -1,15 +1,21 @@
+'use client'
+
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '../ui/modeToggler'
 import Image from 'next/image'
 import logoDark from '../../app/assets/GenDoc_logo_for_dark.png'
+import logoLight from '../../app/assets/GenDoc_logo_for_light.png'
+import { useTheme } from 'next-themes'
 
 const Navbar = () => {
+  const { theme } = useTheme()
+
   return (
     <nav className='flex items-center justify-between p-4 bg-background'>
       <div className='flex items-center'>
         <Link href='/' className='text-2xl font-bold text-foreground'>
-          <Image src={logoDark} width={100} height={100} alt='GenDoc Logo' />
+          <Image src={theme == 'dark' || 'system' ? logoDark : logoLight} width={100} height={100} alt='GenDoc Logo' />
         </Link>
       </div>
       <div className='flex items-center space-x-4'>
