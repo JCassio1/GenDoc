997dc4227bdbac40db817e29da7e7d0218ea651a
JCassio1 <Joselsondias@hotmail.com>
2024-12-08 21:02:33 +0000

    Improve folder structure

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -1,15 +1,9 @@
-import { AppSidebar } from '@/components/app-sidebar'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb'
 import { Separator } from '@/components/ui/separator'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { ModeToggle } from '../ui/modeToggler'
+import { Button } from '../ui/button'
+import { Plus } from 'lucide-react'
+import { AppSidebar } from '../layout/app-sidebar'
 
 export function Dashboard() {
   return (
@@ -23,13 +17,10 @@ export function Dashboard() {
         <header className='sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4'>
           <SidebarTrigger className='-ml-1' />
           <Separator orientation='vertical' className='mr-2 h-4' />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className='hidden md:block'>
-                <BreadcrumbLink href='#'>All Documents</BreadcrumbLink>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+          <Button>
+            <Plus className='mr-1 h-5 w-5' />
+            Create Document
+          </Button>
           <div className='ml-auto'>
             <ModeToggle />
           </div>
